aebc476df25995f46d392c1f0378206a3016cd34
Emanuele Olivetti <olivetti@fbk.eu>
2012-06-16 23:22:42 +0200

    added blending machine

---
+++ blend.py
@@ -0,0 +1,88 @@
+"""Kaggle competition: Predicting a Biological Response.
+
+Blending {RandomForests, ExtraTrees, GradientBoosting} + stretching to
+[0,1].
+
+The predictions are saved in test.csv. The code below created my best
+submission to the competition:
+- public score (25%): 0.43464
+- private score (75%): 0.37751
+- final rank on the private leaderboard: 17th over 711 teams :-)
+
+Note: if you increase the number of estimators of the classifiers,
+e.g. n_estimators=1000, you get a better score/rank on the private
+test set.
+
+Copyright 2012, Emanuele Olivetti.
+BSD license, 3 clauses.
+"""
+
+from __future__ import division
+import numpy as np
+import load_data
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
+def logloss(attempt, actual, epsilon=1.0e-15):
+    """Logloss, i.e. the score of the bioresponse competition.
+    """
+    attempt = np.clip(attempt, epsilon, 1.0-epsilon)
+    return - np.mean(actual * np.log(attempt) + (1.0 - actual) * np.log(1.0 - attempt))
+
+
+if __name__ == '__main__':
+
+    np.random.seed(0) # seed to shuffle the train set
+
+    n_folds = 10
+    verbose = True
+    shuffle = False
+
+    X, y, X_submission = load_data.load()
+
+    if shuffle:
+        idx = np.random.permutation(y.size)
+        X = X[idx]
+        y = y[idx]
+
+    skf = list(StratifiedKFold(y, n_folds))
+
+    clfs = [RandomForestClassifier(n_estimators=100, n_jobs=-1, criterion='gini'),
+            RandomForestClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
+            ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='gini'),
+            ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
+            GradientBoostingClassifier(learn_rate=0.05, subsample=0.5, max_depth=6, n_estimators=50)]
+
+    print "Creating train and test sets for blending."
+    
+    dataset_blend_train = np.zeros((X.shape[0], len(clfs)))
+    dataset_blend_test = np.zeros((X_submission.shape[0], len(clfs)))
+    
+    for j, clf in enumerate(clfs):
+        print j, clf
+        dataset_blend_test_j = np.zeros((X_submission.shape[0], len(skf)))
+        for i, (train, test) in enumerate(skf):
+            print "Fold", i,
+            X_train = X[train]
+            y_train = y[train]
+            X_test = X[test]
+            y_test = y[test]
+            clf.fit(X_train, y_train)
+            y_submission = clf.predict_proba(X_test)[:,1]
+            dataset_blend_train[test, j] = y_submission
+            dataset_blend_test_j[:, i] = clf.predict_proba(X_submission)[:,1]
+        dataset_blend_test[:,j] = dataset_blend_test_j.mean(1)
+
+    print
+    print "Blending."
+    clf = LogisticRegression()
+    clf.fit(dataset_blend_train, y)
+    y_submission = clf.predict_proba(dataset_blend_test)[:,1]
+
+    print "Linear stretch of predictions to [0,1]"
+    y_submission = (y_submission - y_submission.min()) / (y_submission.max() - y_submission.min())
+
+    print "Saving Results."
+    np.savetxt(fname='test.csv', X=y_submission, fmt='%0.9f')
+    
