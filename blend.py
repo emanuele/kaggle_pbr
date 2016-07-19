2097f16e52bbf6942b15356c8a8907ecc973c5d1
Emanuele Olivetti <olivetti@fbk.eu>
2016-07-19 11:34:52 +0200

    fixed learn_rate -> learning_rate (sklearn API) + saved results in submission.csv + minor cleanups to comply PEP8

--- blend.py
+++ blend.py
@@ -27,19 +27,22 @@ from __future__ import division
 import numpy as np
 import load_data
 from sklearn.cross_validation import StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
+
 
 def logloss(attempt, actual, epsilon=1.0e-15):
     """Logloss, i.e. the score of the bioresponse competition.
     """
     attempt = np.clip(attempt, epsilon, 1.0-epsilon)
-    return - np.mean(actual * np.log(attempt) + (1.0 - actual) * np.log(1.0 - attempt))
+    return - np.mean(actual * np.log(attempt) +
+                     (1.0 - actual) * np.log(1.0 - attempt))
 
 
 if __name__ == '__main__':
 
-    np.random.seed(0) # seed to shuffle the train set
+    np.random.seed(0)  # seed to shuffle the train set
 
     n_folds = 10
     verbose = True
@@ -58,13 +61,13 @@ if __name__ == '__main__':
             RandomForestClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
             ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='gini'),
             ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
-            GradientBoostingClassifier(learn_rate=0.05, subsample=0.5, max_depth=6, n_estimators=50)]
+            GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=50)]
 
     print "Creating train and test sets for blending."
-    
+
     dataset_blend_train = np.zeros((X.shape[0], len(clfs)))
     dataset_blend_test = np.zeros((X_submission.shape[0], len(clfs)))
-    
+
     for j, clf in enumerate(clfs):
         print j, clf
         dataset_blend_test_j = np.zeros((X_submission.shape[0], len(skf)))
@@ -75,20 +78,19 @@ if __name__ == '__main__':
             X_test = X[test]
             y_test = y[test]
             clf.fit(X_train, y_train)
-            y_submission = clf.predict_proba(X_test)[:,1]
+            y_submission = clf.predict_proba(X_test)[:, 1]
             dataset_blend_train[test, j] = y_submission
-            dataset_blend_test_j[:, i] = clf.predict_proba(X_submission)[:,1]
-        dataset_blend_test[:,j] = dataset_blend_test_j.mean(1)
+            dataset_blend_test_j[:, i] = clf.predict_proba(X_submission)[:, 1]
+        dataset_blend_test[:, j] = dataset_blend_test_j.mean(1)
 
     print
     print "Blending."
     clf = LogisticRegression()
     clf.fit(dataset_blend_train, y)
-    y_submission = clf.predict_proba(dataset_blend_test)[:,1]
+    y_submission = clf.predict_proba(dataset_blend_test)[:, 1]
 
     print "Linear stretch of predictions to [0,1]"
     y_submission = (y_submission - y_submission.min()) / (y_submission.max() - y_submission.min())
 
     print "Saving Results."
-    np.savetxt(fname='test.csv', X=y_submission, fmt='%0.9f')
-    
+    np.savetxt(fname='submission.csv', X=y_submission, fmt='%0.9f')
